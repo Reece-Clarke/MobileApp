a114ee5ec25d7031e6942bdeea39858f00ea89c9
Reece Clarke <reece.clarke6@studytafensw.edu.au>
2023-11-28 13:11:21 +1100

    MainPage UI layout cleaned up and better refined to represent final app.

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,5 @@
-﻿namespace MobileApp
+﻿
+namespace MobileApp
 {
     public partial class App : Application
     {
@@ -8,5 +9,6 @@
 
             MainPage = new MainPage();
         }
+
     }
 }
--- MauiProgram.cs
+++ MauiProgram.cs
@@ -13,6 +13,9 @@ namespace MobileApp
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Trebuchet-MS.ttf", "Trebuchet");
+                    fonts.AddFont("Trebuchet-MS-Bold.ttf", "TrebuchetBold");
+                    fonts.AddFont("Trebuchet-MS-Italic.ttf", "TrebuchetItalic");
                 });
 
 #if DEBUG
