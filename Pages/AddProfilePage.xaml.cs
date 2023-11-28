47c464025a6b8ce045f1155654072a224cb9071b
Reece Clarke <reece.clarke6@studytafensw.edu.au>
2023-11-28 15:18:57 +1100

    More UI adjustments and additions for MainPage and AddProfilePage.

--- Pages/AddProfilePage.xaml.cs
+++ Pages/AddProfilePage.xaml.cs
@@ -6,4 +6,19 @@ public partial class AddProfilePage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    // Returns user to previous page without adding a profile
+    private async void CancelButton_Clicked(object sender, EventArgs e)
+    {
+        await Navigation.PopModalAsync();
+        SemanticScreenReader.Announce(CancelButton.Text);
+    }
+
+    // Adds a profile to the database and returns user to previous page afterwards
+    private async void AddProfileButton_Clicked(object sender, EventArgs e)
+    {
+        // PLACEHOLDER CODE, only returns user to previous page
+        await Navigation.PopModalAsync();
+        SemanticScreenReader.Announce(AddProfileButton.Text);
+    }
 }
